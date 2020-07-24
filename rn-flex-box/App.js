aff6752a5fdeca2987d9508dbfa8f9cc163d8a55
Roger John William Hobbs <rhobbs@student.wethinkcode.co.za>
2020-07-24 17:36:24 +0200

    init

---
+++ rn-flex-box/App.js
@@ -0,0 +1,40 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
+import { useState } from 'react';
+
+export default function App() {
+	const [inputText, setInputText] = useState('sdf');
+
+	function handlePress(input) {
+		setInputText(input);
+	}
+
+  return (
+    <View style={{padding: 30}}>
+			<View style={sLayOut}>
+				<TextInput 
+					placeholder="course goals"
+					style={sTextInput}
+				/>
+				<Button title="ADD" />
+			</View>
+			<View>
+
+			</View>
+    </View>
+  );
+}
+
+const sTextInput = {
+	borderBottomColor: 'black',
+	borderBottomWidth: 1,
+	padding: 5
+	
+}
+
+const sLayOut = {
+	flexDirection: 'row', 
+	justifyContent: 'space-between',
+	alignItems: 'center'
+}
