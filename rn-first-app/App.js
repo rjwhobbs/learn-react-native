e9771cb408951d624739beccef919caba0a0dad4
Roger John William Hobbs <rhobbs@student.wethinkcode.co.za>
2020-07-25 10:27:02 +0200

    Basic scroll view

--- rn-first-app/App.js
+++ rn-first-app/App.js
@@ -1,41 +1,80 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
+import { StyleSheet, Text, View, Button, TextInput, ScrollView } from 'react-native';
 import { useState } from 'react';
 
 export default function App() {
-	const [inputText, setInputText] = useState('sdf');
+	const [inputText, setInputText] = useState('');
+	const [inputList, setInputList] = useState([]);
 
-	function handlePress(input) {
+	const handleInput = (input) => {
 		setInputText(input);
+	};
+
+	const handleAdd = () => {
+		setInputList(prevInputList => (
+			[...prevInputList, inputText]
+		));
+		setInputText('');
+	};
+
+	const listItems = () => {
+		let items = inputList.map((item, index) => {
+			return (
+				<View
+					key={item + index} 
+					style={styles.listItems}
+				>
+					<Text >{item}</Text>
+				</View> 
+			)
+		});
+	return (<>{items}</>);
 	}
 
   return (
-    <View style={{padding: 30}}>
-			<View style={sLayOut}>
+    <View style={styles.con}>
+			<View style={styles.row}>
 				<TextInput 
+					style={styles.input}
 					placeholder="course goals"
-					style={sTextInput}
+					onChangeText={handleInput} // So we only pass the function to be used, otherwise it actually gets called, eg f() instead of f?
+					value={inputText}
 				/>
-				<Button title="ADD" />
+				<Button 
+					title="ADD" 
+					onPress={handleAdd}
+				/>
 			</View>
-			<View>
-
-			</View>
+			<ScrollView>
+				<View>
+					{listItems()}
+				</View>
+			</ScrollView>
     </View>
-  );
+	);
 }
 
-const sTextInput = {
-	borderBottomColor: 'black',
-	borderBottomWidth: 1,
-	padding: 5
-	
-}
+const styles = StyleSheet.create({
+	con: {
+		padding: 30
+	},
+	input: {
+		borderBottomColor: 'black',
+		borderBottomWidth: 1,
+		padding: 5,
+		width: '80%'
+	},
+	row: {
+		flexDirection: 'row', 
+		justifyContent: 'space-between',	// Align is for opposite of main axis
+		alignItems: 'center'
+	},
+	listItems: {
+		padding: 10,
+		backgroundColor: '#ccc',
+		borderColor: 'black',
+		borderWidth: 1
+	}
+});
 
-const sLayOut = {
-	flexDirection: 'row', 
-	justifyContent: 'space-between',
-	// Align is for opposite of main axis
-	alignItems: 'center'
-}
