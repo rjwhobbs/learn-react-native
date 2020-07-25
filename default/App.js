5379235aee7b3985b7bd3eb0c1e6524db4e2244b
Roger John William Hobbs <rhobbs@student.wethinkcode.co.za>
2020-07-25 18:07:32 +0200

    Made default project for copying

---
+++ default/App.js
@@ -0,0 +1,14 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
+import { useState } from 'react';
+
+export default function App() {
+  return (
+		<View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+});
