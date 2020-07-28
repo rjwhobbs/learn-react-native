bb191274b7c3c19738c906c90fd3633786276608
Roger John William Hobbs <rhobbs@student.wethinkcode.co.za>
2020-07-28 17:23:27 +0200

    some more dimentions

--- second-rn-app/components/GameScreen.js
+++ second-rn-app/components/GameScreen.js
@@ -6,7 +6,8 @@ import {
 	Button,
 	Alert,
 	ScrollView,
-	FlatList
+	FlatList,
+	Dimensions
 } from 'react-native';
 import NumberCon from './NumberCon';
 import Card from './Card';
@@ -112,7 +113,7 @@ const styles = StyleSheet.create({
 	buttonCon: {
 		flexDirection: 'row',
 		justifyContent: 'space-around',
-		marginTop: 20,
+		marginTop: Dimensions.get('window').height > 600 ? 20 : 5,
 		width: 300,
 		maxWidth: '80%'
 	},
@@ -128,7 +129,7 @@ const styles = StyleSheet.create({
 	},
 	listCon: {
 		flex: 1, // Need to add flex 1 to view with nested scroll for android
-		width: '60%', // Add Styles to view not scrollview
+		width:  Dimensions.get('window').width > 350 ? '60%' : '80%', // Add Styles to view not scrollview
 	},
 	list: {
 		flexGrow: 1, // to be able to take as much space but keeps normal behaviour
