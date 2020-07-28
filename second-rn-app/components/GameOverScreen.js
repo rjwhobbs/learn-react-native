85d517d3fd3e07246c23ad6359c2fa7e7f279dce
Roger John William Hobbs <rhobbs@student.wethinkcode.co.za>
2020-07-28 17:54:00 +0200

    code

--- second-rn-app/components/GameOverScreen.js
+++ second-rn-app/components/GameOverScreen.js
@@ -1,34 +1,36 @@
 import React from 'react';
-import {View, Text, StyleSheet, Button, Image} from 'react-native';
+import {View, Text, StyleSheet, Button, Image, Dimensions, ScrollView} from 'react-native';
 import c from '../constants/colours';
 import s from '../constants/styles';
 import MainButton from './MainButton';
 
 const GameOverScreen = (props) => {
 	return (
-		<View style={styles.screen}>
-			<Text>The game is over!</Text>
-			<View style={styles.imageCon}>
-				<Image 
-					fadeDuration={1000} // After caching they won't be faded
-					source={require('../assets/imgs/success.png')} //local
-					// RN cant get width and height form image from the web
-					// source={{uri: 'https://www.yourdictionary.com/images/definitions/lg/12337.summit.jpg'}} 
-					style={styles.image}
-					resizeMode='cover' // defualt
-				/>
+		<ScrollView>
+			<View style={styles.screen}>
+				<Text>The game is over!</Text>
+				<View style={styles.imageCon}>
+					<Image 
+						fadeDuration={1000} // After caching they won't be faded
+						source={require('../assets/imgs/success.png')} //local
+						// RN cant get width and height form image from the web
+						// source={{uri: 'https://www.yourdictionary.com/images/definitions/lg/12337.summit.jpg'}} 
+						style={styles.image}
+						resizeMode='cover' // defualt
+					/>
+				</View>
+				<Text style={{...styles.resultCon, ...s.reg}}> 
+					{/* styles inheret in nested <Text /> */}
+					Number of rounds: 
+					<Text style={s.bold}> {props.rounds} </Text>
+					. Number was: 
+					<Text style={s.bold}> {props.userNum}</Text>
+				</Text>
+				<MainButton onPress={props.onNewGame}>
+					New game
+				</MainButton>
 			</View>
-			<Text style={{...styles.resultCon, ...s.reg}}> 
-				{/* styles inheret in nested <Text /> */}
-				Number of rounds: 
-				<Text style={s.bold}> {props.rounds} </Text>
-				. Number was: 
-				<Text style={s.bold}> {props.userNum}</Text>
-			</Text>
-			<MainButton onPress={props.onNewGame}>
-				New game
-			</MainButton>
-		</View>
+		</ScrollView>
 	);
 }
 
@@ -39,13 +41,13 @@ const styles = StyleSheet.create({
 		alignItems: 'center'
 	},
 	imageCon: {
-		width: 300,
-		height: 300,
-		borderRadius: 150,
+		width: Dimensions.get('window').width * 0.7 ,
+		height: Dimensions.get('window').width * 0.7 ,
+		borderRadius: Dimensions.get('window').width * 0.7 , // border radius should always be half
 		borderWidth: 3,
 		borderColor: c.primary,
 		overflow: 'hidden',
-		marginVertical: 30
+		marginVertical: Dimensions.get('window').height / 30
 	},
 	image: {
 		width: '100%',
@@ -53,7 +55,8 @@ const styles = StyleSheet.create({
 	},
 	resultCon: {
 		marginHorizontal: 30,
-		textAlign: 'center'
+		textAlign: 'center',
+		marginVertical: Dimensions.get('window').height / 60
 	}
 });
 
