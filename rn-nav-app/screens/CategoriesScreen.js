6fbf98fe892cf326baa2dfbb6f2afffa33bb9ad7
Roger John William Hobbs <rhobbs@student.wethinkcode.co.za>
2020-07-30 15:07:46 +0200

    Using navigate method from props from nav screen

--- rn-nav-app/screens/CategoriesScreen.js
+++ rn-nav-app/screens/CategoriesScreen.js
@@ -1,10 +1,17 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, Button } from 'react-native';
 
 const CategoriesScreen = props => {
+	console.log(props);
   return (
     <View style={styles.screen}>
       <Text>The Categories Screen!</Text>
+			<Button 
+				title="Go to meals"
+				onPress={() => {
+					props.navigation.navigate({routeName: 'CategoryMeals'});
+				}}
+			/>
     </View>
   );
 };
