050ab5c6b89ed88b125689c7535e24b1d66920c5
Roger John William Hobbs <rhobbs@student.wethinkcode.co.za>
2020-07-30 09:51:27 +0200

    Nav app init

---
+++ rn-nav-app/App.js
@@ -0,0 +1,15 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
+import { useState } from 'react';
+
+export default function App() {
+  return (
+		<View>
+			<Text>hey vbvbvbvbvbvbv</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+});
