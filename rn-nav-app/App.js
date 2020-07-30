b439c978877a7327ff3943ee04427adca9245e2c
Roger John William Hobbs <rhobbs@student.wethinkcode.co.za>
2020-07-30 14:06:12 +0200

    Can't use {} braces on certain imports

--- rn-nav-app/App.js
+++ rn-nav-app/App.js
@@ -2,32 +2,31 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
 import { useState } from 'react';
-import * as font from 'expo-font';
+import * as Font from 'expo-font';
 import {AppLoading} from 'expo';
+import MealsNavigator from './navigation/MealsNavigator';
 
 const fetchFonts = () => {
-	font.loadAsync({
-		'open-sans': require('./assets/fonts/OpenSans-Regular.ttf'),
-		'open-sans-bold': require('./assets/fonts/OpenSans-Bold.ttf')
-	});
-}
+  return Font.loadAsync({
+    'open-sans': require('./assets/fonts/OpenSans-Regular.ttf'),
+    'open-sans-bold': require('./assets/fonts/OpenSans-Bold.ttf')
+  });
+};
 
 export default function App() {
-	const [fontLaoded, setFontLoaded] = useState(false);
+  const [fontLoaded, setFontLoaded] = useState(false);
 
-	if (!fontLaoded) {
-		return (
-			<AppLoading 
-				startAsync={fetchFonts} 
-				onFinish={() => setFontLoaded(true)}
-			/>
-		);
-	}
+  if (!fontLoaded) {
+    return (
+      <AppLoading
+        startAsync={fetchFonts}
+        onFinish={() => setFontLoaded(true)}
+      />
+    );
+  }
 
   return (
-		<View>
-			<Text>hey vbvbvbvbvbvbv</Text>
-    </View>
+		<MealsNavigator />
   );
 }
 
