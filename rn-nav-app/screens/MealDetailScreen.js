80f4f1e4dfe3f1c873ecdf933b8c4701e3580c8e
Roger John William Hobbs <rhobbs@student.wethinkcode.co.za>
2020-08-03 13:34:28 +0200

    finish getting params

--- rn-nav-app/screens/MealDetailScreen.js
+++ rn-nav-app/screens/MealDetailScreen.js
@@ -1,19 +1,32 @@
 import React from 'react';
 import { View, Text, StyleSheet, Button } from 'react-native';
+import {MEALS} from '../data/dummy-data';
 
 const MealDetailScreen = props => {
+	const mealId = props.navigation.getParam('mealId');
+	const slectedMeal = MEALS.find(meal => mealId === meal.id);
+
   return (
     <View style={styles.screen}>
-      <Text>The Meal Detail Screen!</Text>
+      <Text>{slectedMeal.title}</Text>
 			<Button 
 				title="Go back to catergories"
 				onPress={() => {
-					props.navigation.popToTop(); // Resets stack
+					props.navigation.popToTop(); // Resets stack, ie sends you back to default top
 				}}
 			/>
     </View>
   );
 };
+
+MealDetailScreen.navigationOptions = (navigationData) => {
+	const mealId = navigationData.navigation.getParam('mealId');
+	const selectedMeal = MEALS.find(meal => mealId === meal.id);
+
+	return {
+		headerTitle: selectedMeal.title
+	}
+}
 
 const styles = StyleSheet.create({
   screen: {
