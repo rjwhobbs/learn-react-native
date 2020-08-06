14f5112df33f101304b27eeb9f99d13e66d7306d
Roger John William Hobbs <rhobbs@student.wethinkcode.co.za>
2020-08-06 21:34:36 +0200

    Styling the product detail page

--- rn-shop-app/constants/colours.js
+++ rn-shop-app/constants/colours.js
@@ -2,5 +2,6 @@ export default {
 	primary: '#7ab5d6',
 	accent: '#4f7790',
 	secondary: '#77dd77',
-	white: '#fff'
+	white: '#fff',
+	darkGrey: '#252525'
 };
--- rn-shop-app/screens/shop/ProductDetailScreen.js
+++ rn-shop-app/screens/shop/ProductDetailScreen.js
@@ -10,6 +10,7 @@ import {
 } from 'react-native';	
 import RegText from '../../components/styled/RegText';
 import {useSelector} from 'react-redux';
+import c from '../../constants/colours';
 	
 const ProductDetailScreen = (props) => {	
 	const productId = props.navigation.getParam('productId');
@@ -18,11 +19,25 @@ const ProductDetailScreen = (props) => {
 	);
 
 	return (	
-		<View>
-			<RegText>
-				{selectedProduct.title}
-			</RegText>	
-		</View>	
+		<ScrollView>
+			<Image
+				style={s.image}
+				source={{uri: selectedProduct.imageUrl}}
+			/>
+			<View style={s.button}>
+				<Button
+					color={c.primary}
+					title="Add to cart"
+					onPress={() => {}}
+				/>
+			</View>
+			<RegText style={s.price}>
+				${selectedProduct.price.toFixed(2)}
+			</RegText>
+			<RegText style={s.description}>
+				{selectedProduct.description}
+			</RegText>
+		</ScrollView>	
 	);	
 }	
 
@@ -32,7 +47,27 @@ ProductDetailScreen.navigationOptions = (navData) => {
 	}
 }
 	
-const s = StyleSheet.create({	
+const s = StyleSheet.create({
+	price: {
+		color: c.darkGrey,
+		fontSize: 20,
+		textAlign: 'center',
+		marginVertical: 20
+	},
+	description: {
+		color: c.darkGrey,
+		fontSize: 14,
+		textAlign: 'center',
+		marginHorizontal: 20
+	},
+	image: {
+		width: '100%',
+		height: 300
+	},
+	button: {
+		alignItems: 'center', // the default align is stretch, center 'fixes' that
+		marginVertical: 10
+	}
 });	
 	
 export default ProductDetailScreen;
